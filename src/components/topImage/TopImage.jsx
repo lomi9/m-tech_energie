ed79a879a629aaac4cf3ebdd605d0857240f0b2b
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-01-22 12:24:19 +0100

    chore : add alt on images in topImage components

--- src/components/topImage/TopImage.jsx
+++ src/components/topImage/TopImage.jsx
@@ -5,7 +5,7 @@ export default function TopImage ({ topImage, pageTitle, pageText }) {
     return (
         <section className="topimage">
             <div className="topimage__content">
-                <img src={topImage} className="topimage__content-image" />
+                <img src={topImage} className="topimage__content-image" alt={pageTitle}/>
                 <h1 className="topimage__content-title">
                 {pageTitle}
                 </h1>
